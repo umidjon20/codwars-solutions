0f5d42a1f687e589383cbe4167059d15581c527b
Umid Amirkhulov <uamirqulov5@gmail.com>
2023-02-05 09:23:45 -0700

    new codewars

--- js/script.js
+++ js/script.js
@@ -32,16 +32,16 @@ let abs =[23,-8, 9,12,65,20,88,30]
 
 // bu har 2-elementni olib tashlaydi
 
-let mover = ['bmw','qizil','mers','yashil','gelik','qora']
-// 1.
-function removeEveryOther(arr){
-    let newArr = []
-for (let i = 0; i < arr.length; i += 2) {
-    newArr.push(arr[i]) 
-}
-    return newArr
-}
-  console.log(removeEveryOther(mover))
+// let mover = ['bmw','qizil','mers','yashil','gelik','qora']
+// // 1.
+// function removeEveryOther(arr){
+//     let newArr = []
+// for (let i = 0; i < arr.length; i += 2) {
+//     newArr.push(arr[i]) 
+// }
+//     return newArr
+// }
+//   console.log(removeEveryOther(mover))
 
 // 2.
 //   function removeEveryOther(arr){
@@ -52,15 +52,58 @@ for (let i = 0; i < arr.length; i += 2) {
 // }
 //   console.log(removeEveryOther(mover))
 
-let num1 = [100,200,100]
-let num2 = [10,20,10]
-let total =0
+// let num1 = [100,200,100]
+// let num2 = [10,20,10]
+// let total =0
 
-function arrayPlusArray(arr1, arr2) {
-    let res = arr1.concat(arr2)
-   res.forEach((el,ind) =>{
-      total +=el
-   })
-    return total;
-  }
-  console.log(arrayPlusArray(num1,num2));
+// function arrayPlusArray(arr1, arr2) {
+//     let res = arr1.concat(arr2)
+//    res.forEach((el,ind) =>{
+//       total +=el
+//    })
+//     return total;
+//   }
+//   console.log(arrayPlusArray(num1,num2));
+
+// let arr = "www.codewars.com#about"
+// function removeUrlAnchor(url){
+//  let str = ''
+//   for(let i =0; i< url.length; i++){
+//     if(url[i]==='#')
+//       break
+//     str += url[i]
+//   }
+//   return str
+// }
+// console.log(removeUrlAnchor(arr))
+
+
+let arr = 'abracadabra'
+// function getCount(str) {
+//   let total =0
+//   let vowels = ['a','i','e','o','u']
+
+//   for(i=0; i < str.length;i++){
+//     if(vowels.indexOf(str[i]) > -1){
+      
+//       total++   
+//   }   
+// } return total
+// }
+
+// console.log(getCount(arr));
+
+function getCount(str){
+  let total =0
+   let vowels = ['a','i','e','o','u']
+  let change = str.split('')
+  change.forEach((elm)=>{
+     for(let i =0; i < vowels.length;i++){
+      if(elm === vowels[i]){
+        total ++
+      }
+     }
+  })
+  return total
+}
+console.log(getCount(arr));
